1ad3c21b3d6aefa326bad6a97828f6e285ee4625
dle8 <dle8@u.rochester.edu>
2019-10-24 18:18:39 -0400

    REST APIs to fetch courses & majors data

---
+++ run.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+import json
+
+app = Flask(__name__, static_url_path='/static')
+
+CORS(app)
+
+courses = json.load(open(''))
+majors = json.load(open(''))
+
+
+@app.route('/api/course-list')
+def course_list():
+    return jsonify(sorted([
+        {"name": course['name'], 'code': code, 'credits': course['credit_points']}
+        for code, course in courses.items()], key=lambda course: (int(course['code'][4:8]), course['code'][:4])))
+
+
+@app.route('/api/major-list')
+def major_list():
+    return jsonify([{
+        "name": major['title'],
+        "code": major['code'],
+        "value": major['code'] + ' - ' + major['title'],
+        "label": major['code'] + ' - ' + major['title']
+    } for major in majors])
+
+
+@app.route('/api/course-detail/<course_code>')
+def course_details(course_code):
+    return jsonify(courses[course_code])
+
+
+@app.route('/api/major-detail/<major_code>')
+def major_details(major_code):
+    return jsonify([
+                       major
+                       for major in majors
+                       if major['code'] == major_code
+                   ][0])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
