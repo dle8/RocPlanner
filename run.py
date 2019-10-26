ec65498f2fd219a9f526d6790ad2fc061a6b94d4
Le <dle8@glsn-imac-13.ur.rochester.edu>
2019-10-26 01:00:08 -0400

    Refactor backend API in sync with new data forms

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for, abort, send_from_directory, json, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 import json
 
@@ -7,15 +7,21 @@ app = Flask(__name__, static_url_path='/static')
 
 CORS(app)
 
-
 courses = json.load(open('courses.json'))
-majors = json.load(open('major.json'))
+majors = json.load(open('major_test.json'))
+
 
 @app.route('/api/course-list')
 def course_list():
-    return jsonify(sorted([
-        {"name":course['name'], 'code':code, 'credits':course['credit_points']}
-        for code,course in courses.items()], key=lambda course: (int(course['code'][4:7]), course['code'][:3])))
+    return jsonify(
+        sorted(
+            [
+                {"name": course[0]['name'], 'code': code, 'credits': course[0]['credit_points']}
+                for code, course in courses.items()
+            ], key=lambda course: (str(course['code']).split(" ")[0], int(str(course['code']).split(" ")[1][:-1]))
+        )
+    )
+
 
 @app.route('/api/major-list')
 def major_list():
@@ -24,19 +30,23 @@ def major_list():
         "code": major['code'],
         "value": major['code'] + ' - ' + major['title'],
         "label": major['code'] + ' - ' + major['title']
-        } for major in majors])
+    } for major in majors])
+
 
 @app.route('/api/course-detail/<course_code>')
 def course_details(course_code):
-    return jsonify(courses[course_code]);
+    print(courses[course_code])
+    return jsonify(courses[course_code][0])
+
 
 @app.route('/api/major-detail/<major_code>')
 def major_details(major_code):
     return jsonify([
-        major
-        for major in majors
-        if major['code'] == major_code
-        ][0])
+                       major
+                       for major in majors
+                       if major['code'] == major_code
+                   ][0])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
