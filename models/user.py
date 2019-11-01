c7ddf940a86fd0ff0cea5ea39befb9e1b50dd97d
Le <dle8@car1-imac-17.ur.rochester.edu>
2019-10-31 22:04:11 -0400

    Add confirmation code attr in user model

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 from server import db
 
+
 class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), nullable=False, unique=True)
+    name = db.Column(db.String(256), nullable=False)
     hashed_password = db.Column(db.String(94), nullable=False)
     class_year = db.Column(db.Integer, nullable=False)
 
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmation_code = db.Column(db.Integer)
