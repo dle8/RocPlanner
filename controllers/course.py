9b6450aeadc7e1c8a33380f37fe96fa346f22b98
Dũng Lê <dungle@Dungs-MacBook-Pro.local>
2019-12-09 02:10:12 -0500

    Remove login_required

--- controllers/course.py
+++ controllers/course.py
@@ -8,7 +8,6 @@ courses = json.load(open(os.path.abspath('') + '/dataset/courses.json'))
 
 
 @application.route('/api/course-list')
-@login_required
 def course_list():
     return jsonify(
         [{'name': course[0]['name'], 'code': code, 'credits': course[0]['credit_points']}
@@ -16,6 +15,5 @@ def course_list():
 
 
 @application.route('/api/course-detail/<course_code>')
-@login_required
 def course_details(course_code):
     return jsonify(courses[course_code][0])
